f1f5b8df6fa74ea9fd1231defc4d766f3665df9e
tgermade <tgermade@ethz.ch>
2020-08-20 07:15:43 +0000

    updated regmir variants

--- regmir_tmp/regmir_variants_doRun.R
+++ regmir_tmp/regmir_variants_doRun.R
@@ -147,20 +147,19 @@ loadAll <- function(dea,tp){
 #' @export
 #'
 #' @examples
-regmir.new <- function(e, sets, dea, binary=NULL, alpha=1, use.intercept=FALSE,
-                   keepAll=TRUE, cons=TRUE, KO=TRUE, useFC=TRUE){
+regmir.new <- function(e, sets, dea, sig.binary=FALSE, var.binary=FALSE, cpm.weights=TRUE,
+                       use.intercept=FALSE, keepAll=TRUE, coeff.cons=TRUE, KO=TRUE, useFC=TRUE){
   
   suppressPackageStartupMessages(c(
     library(glmnet),
     library(zetadiv)
   ))
-  if(is.null(binary)) binary <- "score" %in% colnames(sets)
-  
+
   e <- as.data.frame(e)
   e[is.na(e)] <- 1
   
   # generate signal
-  if(binary){
+  if(sig.binary){
     binary.signatures <- list(
       down=.dea2binary(dea, th=0.05, th.alfc=0, restrictSign=-1),
       up=.dea2binary(dea, th=0.05, th.alfc=0, restrictSign=1)
@@ -181,7 +180,7 @@ regmir.new <- function(e, sets, dea, binary=NULL, alpha=1, use.intercept=FALSE,
   
   # gnerate bm
   ## prepare the target matrix
-  if(binary){
+  if(var.binary){
     bm <- sapply(split(as.character(sets$feature), sets$set), FUN=function(x) names(signal) %in% x)
   }else{
     TS2 <- aggregate(sets[,"score",drop=FALSE], by=as.data.frame(sets[,c("set","feature")]), FUN=min)
@@ -200,8 +199,8 @@ regmir.new <- function(e, sets, dea, binary=NULL, alpha=1, use.intercept=FALSE,
     co <- rownames(e[e$FDR<=.05,])
   } else if(sum(e$FDR<=.2)>0){
     co <- rownames(e[e$FDR<=.2,])
-  } else {  # take top 10
-    return(e)
+  } else {
+    co <- rownames(e[1:10,])
   }
   # combine signal, bm and co
   signald <- data.frame( y=signal, bm[,co,drop=FALSE] )
@@ -212,16 +211,33 @@ regmir.new <- function(e, sets, dea, binary=NULL, alpha=1, use.intercept=FALSE,
   }else{
     form <- y~0+.
   }
-  if(binary){
-    mod <- glm.cons( form, data=signald, family="binomial", cons=1, cons.inter=-1)
-  }else{
-    mod <- lm( form, data=signald )
+  if(cpm.weights){
+    w <- dea[rownames(signald),]$logCPM + abs(min(dea[rownames(signald),]$logCPM))
+    if(sig.binary){
+      if(coeff.cons){
+        mod <- glm.cons( form, data=signald, family="binomial", cons=1, cons.inter=-1, weights=w)
+      } else {
+        mod <- glm( form, data=signald, family="binomial", weights=w )
+      }
+    }else{
+      mod <- lm( form, data=signald, weights=w )
+    }
+  } else {
+    if(sig.binary){
+      if(coeff.cons){
+        mod <- glm.cons( form, data=signald, family="binomial", cons=1, cons.inter=-1)
+      } else {
+        mod <- glm( form, data=signald, family="binomial" )
+      }
+    }else{
+      mod <- lm( form, data=signald )
+    }
   }
   
   # we extract the coefficients and p-values, and reorganize the output:
   res <- coef(summary(mod))
   res <- res[order(res[,4]),,drop=FALSE]
-  colnames(res) <- c("beta","stderr",ifelse(binary,"z","t"),"pvalue")
+  colnames(res) <- c("beta","stderr",ifelse(sig.binary,"z","t"),"pvalue")
   res <- res[grep("^\\(Intercept\\)$|FALSE$", row.names(res), invert=TRUE),,drop=FALSE]
   row.names(res) <- gsub("TRUE","",row.names(res))
   
@@ -229,7 +245,7 @@ regmir.new <- function(e, sets, dea, binary=NULL, alpha=1, use.intercept=FALSE,
     co2 <- setdiff(colnames(bm), co)
     co2 <- data.frame(row.names=co2, beta=rep(NA_real_,length(co2)),
                       stderr=NA_real_, z=NA_real_, pvalue=1)
-    if(!binary) colnames(co2)[3] <- "t"
+    if(!sig.binary) colnames(co2)[3] <- "t"
     res <- rbind(res,co2)
   }
   
@@ -257,70 +273,154 @@ TPs <- c(
 )
 
 # load
-dea <- loadAll("../master_19_20/enrichMiR_benchmark/data/amin.DEA.SE.rds",TPs)
+dea <- loadAll("../master_19_20/enrichMiR_benchmark/data/bartel.hek.DEA.SE.rds",TPs)
 dea <- lapply(dea, cleanDEA)
-e.list <- readRDS("../master_19_20/enrichMiR_benchmark/results/amin.enrichMiR.rds")
-TS <- readRDS("../master_19_20/enrichMiR_benchmark/data/TargetScan_mouse.rds")
+e.list <- readRDS("../master_19_20/enrichMiR_benchmark/results/bartel.hek.enrichMiR.rds")
+TS <- readRDS("../master_19_20/enrichMiR_benchmark/data/TargetScan_human.rds")
 if(all(c("family", "feature") %in% colnames(TS))){
   colnames(TS) <- gsub("^family$","set", colnames(TS))
 }
 TS <- DataFrame(TS)
 
-# do logistic GLM with coeff constr
-print("logistic run...")
-e.logistic <- lapply(names(dea), function(i) lapply(e.list[[i]]$original@res, function(e){
-  print("testing...")
-  regmir.new(e, TS, dea[[i]], KO=TRUE, binary=TRUE) }))
-saveRDS(e.logistic, "results2/regmir.amin.log.rds")
-# do logistic GLM without coeff constr
-print("logistic no-constraint run...")
-e.logistic.nc <- lapply(names(dea), function(i) lapply(e.list[[i]]$original@res, function(e) 
-  regmir.new(e, TS, dea[[i]], KO=TRUE, binary=TRUE, cons=FALSE) ))
-saveRDS(e.logistic.nc, "results2/regmir.amin.lognc.rds")
-# do non-logistic GLM
-print("non-logistic run...")
-e.gaussian <- lapply(names(dea), function(i) lapply(e.list[[i]]$original@res, function(e) 
-  regmir.new(e, TS, dea[[i]], KO=TRUE, binary=FALSE) ))
-saveRDS(e.gaussian, "results2/regmir.amin.nonlog.rds")
 
 
 
-log <- readRDS("results2/regmir.amin.log.rds")
-lognc <- readRDS("results2/regmir.amin.lognc.rds")
-nonlog <- readRDS("results2/regmir.amin.nonlog.rds")
+for(w in cpm.weights <- c(T,F)){
+  print(paste("weights:", w))
+  W=ifelse(w,"w","nw")
+  
+  for(v in var.bin <- c(T,F)){
+    print(paste("binary variables:", v))
+    V=ifelse(v,"bin","cont")
+    
+    for(s in sig.bin <- c(T,F)){
+      print(paste("binary signal:", s))
+      S=ifelse(s,"bin","cont")
+      
+      if(s){
+        for(c in coeff.cons <- c(T,F)){
+          print(paste("constrained coefficients:", c))
+          C=ifelse(c,"c","nc")
+          
+          e <- lapply(names(dea), function(i) lapply(e.list[[i]]$original@res, function(e) 
+            regmir.new(e, TS, dea[[i]], KO=FALSE, cpm.weights=w, var.binary=v, sig.binary=s, coeff.cons=c) 
+            ))
+          saveRDS(e, paste0("results2/bartel.hek.regmir.", S,"_",V,".",W,".",C,".rds") )
+          print( paste(paste0("bartel.hek.regmir.", S,"_",V,".",W,".",C,".rds"),"saved.") )
+          
+        }
+      } else {
 
-names(log) <- names(lognc) <- names(nonlog) <- names(e.list)
-
-d1 <- data.frame(p.rank=sapply(e.list$`218DKOvWT`$original@res, function(x) which(rownames(x)==TPs[names(log)])),
-           p.sig=sapply(e.list$`218DKOvWT`$original@res, function(x) x$FDR[rownames(x)==TPs[names(log)]]<=.05),
-           orig="original")
-d2 <- data.frame(p.rank=sapply(log$`218DKOvWT`, function(x) which(rownames(x)==TPs[names(log)])),
-           p.sig=sapply(log$`218DKOvWT`, function(x) x$FDR[rownames(x)==TPs[names(log)]]<=.05),
-           orig="regmir.logistic")
-d3 <- data.frame(p.rank=sapply(lognc$`218DKOvWT`, function(x) which(rownames(x)==TPs[names(lognc)])),
-           p.sig=sapply(lognc$`218DKOvWT`, function(x) x$FDR[rownames(x)==TPs[names(log)]]<=.05),
-           orig="regmir.logistic.nc")
-d4 <- data.frame(p.rank=sapply(nonlog$`218DKOvWT`, function(x) which(rownames(x)==TPs[names(nonlog)])),
-           p.sig=sapply(nonlog$`218DKOvWT`, function(x) x$FDR[rownames(x)==TPs[names(log)]]<=.05),
-           orig="regmir.nonlogistic")
-d <-rbind(d1,d2,d3,d4)
-d$method <- rownames(d)[1:nrow(d1)]
-for(x in colnames(d)[-1]){
-  d[[x]] <- as.factor(d[[x]])
+        e <- lapply(names(dea), function(i) lapply(e.list[[i]]$original@res, function(e) 
+          regmir.new(e, TS, dea[[i]], KO=FALSE, cpm.weights=w, var.binary=v, sig.binary=s) 
+          ))
+        saveRDS(e, paste0("results2/bartel.hek.regmir.", S,"_",V,".",W,".rds") )
+        print( paste(paste0("bartel.hek.regmir.", S,"_",V,".",W,".rds"),"saved.") )
+        
+      }
+    }
+  }
 }
-d$method <- droplevels(d$method)
-
-ggplot(d,aes(method,p.rank,color=orig,shape=p.sig)) + geom_point(size=3) + scale_y_sqrt() + coord_flip()
 
 
-d1 <- getBench(e.list, TPs[names(log)], perm = TRUE)
-d2 <- getBench(log, TPs[names(log)], e.obj = FALSE)
-d3 <- getBench(lognc, TPs[names(log)], e.obj = FALSE)
-d4 <- getBench(nonlog, TPs[names(log)], e.obj = FALSE)
-d1 <- d1[d1$prop.rep=="original",-c(2,9)]
 
-df <- dplyr::bind_rows(list(original=d1,logistic=d2,logistic.nc=d3,nonlogistic=d4), .id="orig")
 
-TPvFP(df, label.all=TRUE, features = "orig")
-HM(df, form=method~orig)
-HM(df, metric="FP.atFDR05", form=method~orig)
+# for(w in cpm.weights <- c(T,F)){
+#   print(paste("weights:", w))
+#   W=ifelse(w,"w","nw")
+#   
+#   for(v in var.bin <- c(T,F)){
+#     print(paste("binary variables:", v))
+#     V=ifelse(v,"bin","cont")
+#     
+#     for(s in sig.bin <- c(T,F)){
+#       print(paste("binary signal:", s))
+#       S=ifelse(s,"bin","cont")
+#       
+#       if(s){
+#         for(c in coeff.cons <- c(T,F)){
+#           print(paste("constrained coefficients:", c))
+#           C=ifelse(c,"c","nc")
+#           
+#           
+#           print(paste0("results/amin.regmir.", S,"_",V,".",W,".",C,".rds") )
+#           print( paste(paste0("results/amin.regmir.", S,"_",V,".",W,".",C,".rds"),"saved.") )
+#         }
+#       } else {
+# 
+#         print(paste0("results/amin.regmir.", S,"_",V,".",W,".rds") )
+#         print( paste(paste0("results/amin.regmir.", S,"_",V,".",W,".rds"),"saved.") )
+#       }
+#     }
+#   }
+# }
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# # do logistic GLM with coeff constr
+# print("logistic run...")
+# e.logistic <- lapply(names(dea), function(i) lapply(e.list[[i]]$original@res, function(e){
+#   print("testing...")
+#   regmir.new(e, TS, dea[[i]], KO=TRUE, binary=TRUE) }))
+# saveRDS(e.logistic, "results2/regmir.amin.log.rds")
+# 
+# # do logistic GLM without coeff constr
+# print("logistic no-constraint run...")
+# e.logistic.nc <- lapply(names(dea), function(i) lapply(e.list[[i]]$original@res, function(e) 
+#   regmir.new(e, TS, dea[[i]], KO=TRUE, binary=TRUE, cons=FALSE) ))
+# saveRDS(e.logistic.nc, "results2/regmir.amin.lognc.rds")
+# 
+# # do non-logistic GLM
+# print("non-logistic run...")
+# e.gaussian <- lapply(names(dea), function(i) lapply(e.list[[i]]$original@res, function(e) 
+#   regmir.new(e, TS, dea[[i]], KO=TRUE, binary=FALSE) ))
+# saveRDS(e.gaussian, "results2/regmir.amin.nonlog.rds")
+# 
+# 
+# 
+# log <- readRDS("results2/regmir.amin.log.rds")
+# lognc <- readRDS("results2/regmir.amin.lognc.rds")
+# nonlog <- readRDS("results2/regmir.amin.nonlog.rds")
+# 
+# names(log) <- names(lognc) <- names(nonlog) <- names(e.list)
+# 
+# d1 <- data.frame(p.rank=sapply(e.list$`218DKOvWT`$original@res, function(x) which(rownames(x)==TPs[names(log)])),
+#            p.sig=sapply(e.list$`218DKOvWT`$original@res, function(x) x$FDR[rownames(x)==TPs[names(log)]]<=.05),
+#            orig="original")
+# d2 <- data.frame(p.rank=sapply(log$`218DKOvWT`, function(x) which(rownames(x)==TPs[names(log)])),
+#            p.sig=sapply(log$`218DKOvWT`, function(x) x$FDR[rownames(x)==TPs[names(log)]]<=.05),
+#            orig="regmir.logistic")
+# d3 <- data.frame(p.rank=sapply(lognc$`218DKOvWT`, function(x) which(rownames(x)==TPs[names(lognc)])),
+#            p.sig=sapply(lognc$`218DKOvWT`, function(x) x$FDR[rownames(x)==TPs[names(log)]]<=.05),
+#            orig="regmir.logistic.nc")
+# d4 <- data.frame(p.rank=sapply(nonlog$`218DKOvWT`, function(x) which(rownames(x)==TPs[names(nonlog)])),
+#            p.sig=sapply(nonlog$`218DKOvWT`, function(x) x$FDR[rownames(x)==TPs[names(log)]]<=.05),
+#            orig="regmir.nonlogistic")
+# d <-rbind(d1,d2,d3,d4)
+# d$method <- rownames(d)[1:nrow(d1)]
+# for(x in colnames(d)[-1]){
+#   d[[x]] <- as.factor(d[[x]])
+# }
+# d$method <- droplevels(d$method)
+# 
+# ggplot(d,aes(method,p.rank,color=orig,shape=p.sig)) + geom_point(size=3) + scale_y_sqrt() + coord_flip()
+# 
+# 
+# d1 <- getBench(e.list, TPs[names(log)], perm = TRUE)
+# d2 <- getBench(log, TPs[names(log)], e.obj = FALSE)
+# d3 <- getBench(lognc, TPs[names(log)], e.obj = FALSE)
+# d4 <- getBench(nonlog, TPs[names(log)], e.obj = FALSE)
+# d1 <- d1[d1$prop.rep=="original",-c(2,9)]
+# 
+# df <- dplyr::bind_rows(list(original=d1,logistic=d2,logistic.nc=d3,nonlogistic=d4), .id="orig")
+# 
+# TPvFP(df, label.all=TRUE, features = "orig")
+# HM(df, form=method~orig)
+# HM(df, metric="FP.atFDR05", form=method~orig)
